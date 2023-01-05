8e86f40187720f70e5ecdcc217f8dfdc323469f4
chrishazfun <chrishazfun@users.noreply.github.com>
2023-01-05 23:44:09 +1100

    adjusted yay command to force install with no confirmation, adjusted spacing and added more echoed indicators for progress

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # installarch
+My method of installing Arch Linux, using a script that edits a config then uses the pre-installed archinstall command with it, more or less making my own distro.
 
-my method of installing arch linux, using a script that edits a config then uses the pre-installed archinstall command
-
-### installation
+## Installation
 ```
 sudo pacman -Syy git
 git clone https://github.com/chrishazfun/installarch
 cd installarch
 bash ./go.sh
 ```
+
+## Notes
+Premade configurations used when executing ./go.sh doesn't include disk layouts or conditions for root access and sudo access for users, you must manually configure that.
--- go.sh
+++ go.sh
@@ -1,28 +1,40 @@
 #!/bin/bash
-# bash ./go.sh
+# bash ./go.sh OR archinstall --config config.json
 if [ -f "/etc/arch-release" ]; then
-	# update keyrings and archinstall pkg to latest to prevent packages failing to install
+
+	echo "Updating keyrings and archinstall to latest to prevent packages failing to install"
 	sudo pacman -S --noconfirm --needed archinstall archlinux-keyring
-	gpu_type=$(lspci)
-	if grep -E "NVIDIA|GeForce" <<< ${gpu_type}; then
-		echo "gpu detected as nvidia"
-		sleep 1
+
+	echo "Enabling parallel downloads, using all threads for better compilation"
+	sed -i 's/^#ParallelDownloads = 5/ParallelDownloads = 20/' /etc/pacman.conf
+	NC=$(grep -c ^processor /proc/cpuinfo)
+	sed -i "s/#MAKEFLAGS=\"-j2\"/MAKEFLAGS=\"-j$NC\"/g" /etc/makepkg.conf
+	sed -i "s/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T $NC -z -)/g" /etc/makepkg.conf
+
+	echo "Adjusting GPU drivers"
+	GPU_TYPE=$(lspci)
+	if grep -E "NVIDIA|GeForce" <<< ${GPU_TYPE}; then
+		echo "GPU detected as Nvidia"
 		sed -i 's/#gfx_driver/"gfx_driver": "Nvidia",/' config.json
 	else
-		echo "falling back to open-source drivers"
+		echo "Not Nvidia, falling back to open-source drivers"
 		sleep 1
 		sed -i 's/#gfx_driver/"gfx_driver": "All open-source (default)",/' config.json
 	fi
+
+	# select disk/s
 	lsblk -n
-	read -p "please enter primary installation disk (e.g. /dev/sda /dev/nvme0n1): " harddrive
-	if [[ "${harddrive,,}" =~ (\/\S*) ]]; then
-		sed -i "s|#harddrives|\"harddrives\": [\"$harddrive\"]|g" config.json
-		echo "installing in 3.. 2.. 1.." && sleep 2
+	read -p "Please enter primary installation disk (e.g. /dev/sda /dev/nvme0n1): " drive
+	if [[ "${drive,,}" =~ (\/\S*) ]]; then
+		sed -i "s|#harddrives|\"harddrives\": [\"$drive\"]|g" config.json
+		echo "Installing with partly generated config in 3.. 2.. 1.." && sleep 2
 		archinstall --config config.json
+		echo "Done!... hopefully"
 	else
-		echo "invalid disk format, exiting script"
+		echo "Invalid disk format, exiting script"
 		exit 0
 	fi
+
 else
-	echo "this ain't arch *flies away*"
+	echo "This isn't Arch *flies away*"
 fi
