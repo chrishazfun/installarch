1b8beefaea532660919958122bfcf910eca1f2a6
chrishazfun <chrishazfun@users.noreply.github.com>
2023-01-05 21:15:41 +1100

    adjusting config, changing regex for config changes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # installarch
+
+my method of installing arch linux, using a script that edits a config then uses the pre-installed archinstall command
--- go.sh
+++ go.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 # bash ./go.sh
 if [ -f "/etc/arch-release" ]; then
-	sudo pacman -S --noconfirm archinstall archlinux-keyring # update keyrings and archinstall pkg to latest to prevent packages failing to install
-	echo "checking gpu and adjusting accordingly"
+	# update keyrings and archinstall pkg to latest to prevent packages failing to install
+	sudo pacman -S --noconfirm --needed archinstall archlinux-keyring
 	gpu_type=$(lspci)
 	if grep -E "NVIDIA|GeForce" <<< ${gpu_type}; then
 		echo "gpu detected as nvidia"
 		sleep 1
-		sed -i 's/"gfx_driver": "",/"gfx_driver": "Nvidia",/' config.json
+		sed -i 's/#gfx_driver/"gfx_driver": "Nvidia",/' config.json
 	else
-		echo "not nvidia, falling back to open-source drivers"
+		echo "falling back to open-source drivers"
 		sleep 1
-		sed -i 's/"gfx_driver": "",/"gfx_driver": "All open-source (default)",/' config.json
+		sed -i 's/#gfx_driver/"gfx_driver": "All open-source (default)",/' config.json
 	fi
-	lsblk
+	lsblk -n
 	read -p "please enter primary installation disk (e.g. /dev/sda /dev/nvme0n1): " harddrive
 	if [[ "${harddrive,,}" =~ (\/\S*) ]]; then
-		sed -i "s|\"harddrives\": [\"\"],|\"harddrives\": [\"$harddrive\"],|g" config.json
+		sed -i "s|#harddrives|\"harddrives\": [\"$harddrive\"],|g" config.json
 		echo "installing in 3.. 2.. 1.." && sleep 2
 		archinstall --config config.json
 	else
