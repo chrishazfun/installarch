4c55e93a61f2a4c35268ea565c2b2359fe83724f
chrishazfun <chrishazfun@users.noreply.github.com>
2023-02-10 00:52:35 +1100

    updates to config, clearing out post.sh and disabling mirror check ahead of installation

--- go.sh
+++ go.sh
@@ -2,13 +2,17 @@
 # bash ./go.sh
 if [ -f "/etc/arch-release" ]; then
 
-	echo "Enabling parallel downloads"
+	echo "Enabling parallel downloads, color and ignoring lxqt-themes pkg in favor of single lxqt-less-theme-git theme"
 	sleep 1
 	sed -i 's/^#ParallelDownloads = 5/ParallelDownloads = 25/' /etc/pacman.conf
+	sed -i 's/^#Color/Color/' /etc/pacman.conf
+	sed -i 's/^#IgnorePkg   =/IgnorePkg=lxqt-themes/' /etc/pacman.conf
 
-	echo "Updating database and checking for updates specific to archlinux-keyring and archinstall, silently skipping if they're already up to date"
+	echo "Updating database and checking for updates specific to archlinux-keyring, archinstall and reflector, silently skipping if they're already up to date"
 	sleep 1
 	sudo pacman -Syy --needed archlinux-keyring archinstall reflector
+
+	echo "Seperately scanning for optimal mirrors, disabled mirror check in archinstall config as its not returning great results"
 	reflector -a 48 -c Australia,Sydney -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
 
 	echo "Installing with partly generated config in 3.. 2.. 1.." && sleep 2
--- post.sh
+++ post.sh
@@ -1,26 +1,8 @@
 # not as important but still worth putting into a file
-# bash ./post.sh when in the cloned repo dir after rebooting into system
+# bash ./post.sh when on the other end
 
-reflector -a 48 -c Australia,Sydney -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
-
-echo "Hiding LSP and ZAM plugins from application menu"
+# hiding lsp and zam plugins from application menu
 echo "[Desktop Entry]
 Hidden=true" > /tmp/1
 find /usr -name "*lsp_plug*desktop" 2>/dev/null | cut -f 5 -d '/' | xargs -I {} cp /tmp/1 ~/.local/share/applications/{}
 find /usr -name "*zam*desktop" 2>/dev/null | cut -f 5 -d '/' | xargs -I {} cp /tmp/1 ~/.local/share/applications/{}
-
-echo "Installing Yay AUR helper"
-git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin
-makepkg -si
-cd
-
-echo "Installing AUR packages"
-yay -S --noconfirm nile-git kdocker pamac-nosnap shutter-encoder microsoft-edge-stable-bin chromium-widevine sublime-text-4 xboxdrv protonup-ng-git ttf-ms-fonts
-
-echo "Installing flatpaks"
-flatpak install -y --noninteractive org.firestormviewer.FirestormViewer com.steamgriddb.steam-rom-manager
-
-echo "Adding bash aliases"
-curl https://chrishaz.fun/arch/.bashrc >> ~/.bashrc
-source ~/.bashrc
