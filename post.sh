b38d99d3b376deb7b805f5625a6d76d91bd005f2
chrishazfun <chrishazfun@users.noreply.github.com>
2023-02-16 01:23:36 +1100

    removed yay command from post.sh, config.json added aur pkgs

--- post.sh
+++ post.sh
@@ -7,4 +7,4 @@ Hidden=true" > /tmp/1
 find /usr -name "*lsp_plug*desktop" 2>/dev/null | cut -f 5 -d '/' | xargs -I {} cp /tmp/1 ~/.local/share/applications/{}
 find /usr -name "*zam*desktop" 2>/dev/null | cut -f 5 -d '/' | xargs -I {} cp /tmp/1 ~/.local/share/applications/{}
 
-yay -S chromium-widevine vscodium-bin darling-bin stable-diffusion-ui lxqt-less-theme-git kdocker pamac-nosnap shutter-encoder xboxdrv protonup-ng-git ttf-ms-fonts
+yay -S chromium-widevine darling-bin stable-diffusion-ui lxqt-less-theme-git kdocker pamac-nosnap shutter-encoder xboxdrv protonup-ng-git ttf-ms-fonts
