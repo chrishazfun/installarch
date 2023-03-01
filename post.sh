7c5a3d69186024b071f83cca47e3e79bb979d2f2
chrishazfun <chrishazfun@users.noreply.github.com>
2023-03-02 04:48:33 +1100

    moved yay command to post.sh, added protonup gui and appimage spec to config

--- post.sh
+++ post.sh
@@ -6,3 +6,10 @@ echo "[Desktop Entry]
 Hidden=true" > /tmp/1
 find /usr -name "*lsp_plug*desktop" 2>/dev/null | cut -f 5 -d '/' | xargs -I {} cp /tmp/1 ~/.local/share/applications/{}
 find /usr -name "*zam*desktop" 2>/dev/null | cut -f 5 -d '/' | xargs -I {} cp /tmp/1 ~/.local/share/applications/{}
+
+git clone https://aur.archlinux.org/yay-bin /tmp/yay-bin
+cd /tmp/yay-bin
+makepkg -si
+cd
+
+yay -S --noconfirm darling-bin sublime-text-4 stable-diffusion-ui kdocker shutter-encoder xboxdrv ttf-ms-fonts
