9fbe3f29fd26b562eb0b438df6678cb9605f4573
chrishazfun <chrishazfun@users.noreply.github.com>
2023-03-22 04:44:18 +1100

    updates: might have post install script working now

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-# bash ./go.sh
+
+# bash ./install.sh
 if [ -f "/etc/arch-release" ]; then
 
-	echo "Enabling parallel downloads, color in the terminal and ignoring lxqt-themes pkg in favor of single lxqt-less-theme-git theme"
+	echo "initializing pacmans keyring"
+	sleep 1
+	pacman-key --init
+
+	echo "Enabling parallel downloads, color in the terminal and blocking xterm from being installed in this config, just in case"
 	sleep 1
 	sed -i 's/^#ParallelDownloads = 5/ParallelDownloads = 25/' /etc/pacman.conf
 	sed -i 's/^#Color/Color/' /etc/pacman.conf
@@ -10,7 +15,7 @@ if [ -f "/etc/arch-release" ]; then
 
 	echo "Updating database and checking for updates specific to archlinux-keyring, archinstall and reflector, silently skipping if they're already up to date"
 	sleep 1
-	sudo pacman -Syy --needed archlinux-keyring archinstall reflector
+	sudo pacman -Syy --needed archlinux-keyring archinstall reflector python-setuptools
 
 	#echo "Seperately scanning for optimal mirrors, disabled mirror check in archinstall config as its not returning great results"
 	#reflector -a 48 -c Australia,Sydney -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
--- post.sh
+++ post.sh
@@ -1,8 +1,37 @@
-# not as important but still worth putting into a file
-# bash ./post.sh when on the other end
+#!/bin/bash
 
 # hiding lsp and zam plugins from application menu
+read 
 echo "[Desktop Entry]
 Hidden=true" > /tmp/1
 find /usr -name "*lsp_plug*desktop" 2>/dev/null | cut -f 5 -d '/' | xargs -I {} cp /tmp/1 ~/.local/share/applications/{}
 find /usr -name "*zam*desktop" 2>/dev/null | cut -f 5 -d '/' | xargs -I {} cp /tmp/1 ~/.local/share/applications/{}
+
+# pacman tweaks, blocking xterm as it's a common optional dep that we don't need
+sed -i 's/^#ParallelDownloads = 5/ParallelDownloads = 25/' /etc/pacman.conf
+sed -i 's/^#IgnorePkg   =/IgnorePkg=xterm/' /etc/pacman.conf
+sed -i 's/^#Color/Color/' /etc/pacman.conf
+
+# cloning yay-bin in the chrooted post-install script instead of the custom-commands like we COULD but can't rn
+git clone https://aur.archlinux.org/yay-bin /tmp/yay-bin
+cd /tmp/yay-bin
+makepkg -si
+cd
+
+# getting them aur pkgs
+yay -S --noconfirm --needed darling-bin kdocker xboxdrv ttf-ms-fonts spacecadetpinball-git shutter-encoder octopi chromium-widevine
+
+# getting some flatpaks, minecraft bedrock for linux anyone?
+flatpak install -y --noninteractive net.davidotek.pupgui2 com.heroicgameslauncher.hgl io.itch.itch net.brinkervii.grapejuice io.mrarm.mcpelauncher com.gitlab.JakobDev.jdMinecraftLauncher com.microsoft.Edge com.github.tchx84.Flatseal io.podman_desktop.PodmanDesktop io.github.vikdevelop.SaveDesktop
+
+# setting the awesomewm config
+mkdir -p ~/.config/awesome/
+cp /etc/xdg/awesome/rc.lua ~/.config/awesome/
+sed -i 's/^terminal = \"xterm\"/terminal = \"qterminal\"/' ~/.config/awesome/rc.lua
+sed -i 's/^editor = os.getenv(\"EDITOR\") or \"nano\"/editor = os.getenv(\"EDITOR\") or \"vim\"/' ~/.config/awesome/rc.lua
+
+# setting mpv config, might set some things idk
+cp -r /usr/share/doc/mpv/ ~/.config/
+
+# scrolling fix (temporary)
+xset r rate 200 30
