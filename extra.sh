6046e165d20959bc244741d4cb12ebecdb86a115
chrishazfun <chrishazfun@users.noreply.github.com>
2023-05-13 22:11:39 +1000

    moved some aur pkgs back to extra.sh, yay-bin is still in the config

--- extra.sh
+++ extra.sh
@@ -6,5 +6,7 @@ Hidden=true" > /tmp/1
 find /usr -name "*lsp_plug*desktop" 2>/dev/null | cut -f 5 -d '/' | xargs -I {} cp /tmp/1 ~/.local/share/applications/{}
 find /usr -name "*zam*desktop" 2>/dev/null | cut -f 5 -d '/' | xargs -I {} cp /tmp/1 ~/.local/share/applications/{}
 
+yay -S appimagelauncher ungoogled-chromium-xdg-bin chromium-widevine chromium-extension-web-store bitwarden-chromium protonup-qt-bin chiaki itch-setup-bin heroic-games-launcher-bin mcbelauncher-bin xbox-xcloud bleachbit-admin darling-bin flowblade kdocker xboxdrv ttf-ms-fonts shutter-encoder octopi flatseal devtoolbox powershell-bin
+
 # yay -S --needed --noconfirm greenlight-bin
 # ^ eventually probably as xbox-xcloud may be been renamed to greenlight at some point
