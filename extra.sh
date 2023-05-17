250a1e4999f5cf9f10bf36e07a14fe0ba5e2b2e3
chrishazfun <chrishazfun@users.noreply.github.com>
2023-05-18 09:08:45 +1000

    really don't wanna use extra.sh, putting commands into custom-commands again

--- extra.sh
+++ extra.sh
@@ -1,27 +1,3 @@
 #!/bin/bash
-
-echo "# pacman stuff"
-sed -i 's/^#ParallelDownloads = 5/ParallelDownloads = 25/' /etc/pacman.conf
-sed -i 's/^#IgnorePkg   =/IgnorePkg=xterm/' /etc/pacman.conf
-sed -i 's/^#Color/Color/' /etc/pacman.conf
-
-
-echo "# installing easyeffects, lsp and zam plugins and scrubing them from the app menu"
-pacman -S easyeffects calf mda.lv2 lsp-plugins
-echo "[Desktop Entry]
-Hidden=true" > /tmp/1
-find /usr -name "*lsp_plug*desktop" 2>/dev/null | cut -f 5 -d '/' | xargs -I {} cp /tmp/1 ~/.local/share/applications/{}
-find /usr -name "*zam*desktop" 2>/dev/null | cut -f 5 -d '/' | xargs -I {} cp /tmp/1 ~/.local/share/applications/{}
-
-echo "# installing qemu/virt-manager, sed'ing config to work"
-pacman -S qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils libguestfs ovmf swtpm openbsd-netcat
-sed -i 's/^#unix_sock_group = \"libvirt\"/unix_sock_group = \"libvirt\"/' /etc/libvirt/libvirtd.conf
-sed -i 's/^#unix_sock_ro_perms = \"0777\"/unix_sock_ro_perms = \"0777\"/' /etc/libvirt/libvirtd.conf
-sed -i 's/^#unix_sock_rw_perms = \"0770\"/unix_sock_rw_perms = \"0770\"/' /etc/libvirt/libvirtd.conf
-usermod -aG libvirt $USERNAME
-systemctl enable libvirtd
-
-yay -S appimagelauncher ungoogled-chromium-xdg-bin chromium-widevine chromium-extension-web-store bitwarden-chromium protonup-qt-bin chiaki itch-setup-bin heroic-games-launcher-bin mcbelauncher-bin xbox-xcloud bleachbit-admin darling-bin flowblade kdocker xboxdrv ttf-ms-fonts shutter-encoder octopi flatseal devtoolbox powershell-bin
-
 # yay -S --needed --noconfirm greenlight-bin
 # ^ eventually probably as xbox-xcloud may be been renamed to greenlight at some point
--- install.sh
+++ install.sh
@@ -12,70 +12,76 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-# Check if the /etc/arch-release file exists
 if [ ! -f "/etc/arch-release" ]; then
    echo "This script is only intended to run on Arch Linux"
    exit 1
 fi
 
-# Kill gpg-agent processes and empty out pacmans gnupg directory
 echo "Killing gpg-agent processes and emptying out pacmans gnupg directory"
 sleep 2
 killall gpg-agent
 rm -rf /etc/pacman.d/gnupg/*
 
-# Initialize and populate pacmans keyring
 echo "Initializing and populating pacmans keyring"
 sleep 2
 pacman-key --init || { echo "Failed to initialize pacmans keyring"; exit 1; }
 pacman-key --populate archlinux || { echo "Failed to populate pacmans keyring"; exit 1; }
 
-# Disable timeout on downloading packages, enable parallel downloads and set them to 25 max, enable color in the terminal and block xterm from being installed
-echo "Disabling timeout on downloading packages, enabling parallel downloads and setting them to 25 max, enabling color in the terminal and blocking xterm from being installed"
+echo "Disabling timeout on downloading packages"
 sleep 2
 if ! perl -pi -e '$_ .= qq(DisableDownloadTimeout\n) if /# Misc options/' /etc/pacman.conf; then
     echo "Failed to disable download timeout"
     exit 1
 fi
 
+echo "Enabling parallel downloads and setting them to 25 max"
 if ! sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 25/' /etc/pacman.conf; then
     echo "Failed to enable parallel downloads"
     exit 1
 fi
 
+echo "Enabling color in the terminal"
 if ! sed -i 's/#Color/Color/' /etc/pacman.conf; then
     echo "Failed to enable color in the terminal"
     exit 1
 fi
 
+echo "Adding xterm to the pkg blocklist"
 if ! sed -i 's/#IgnorePkg   =/IgnorePkg=xterm/' /etc/pacman.conf; then
     echo "Failed to add xterm to the blocklist so as to prevent being installed when archinstall is executed"
     exit 1
 fi
 
-# Prompt for username and password
-#read -p "Enter username: " un
-#sed -i "s/#username/$un/" creds.json || { sed -i "s/$un/#username/" creds.json; echo "Failed to update username in creds.json, reverted username in file back to dummy."; exit 1; }
-#sed -i "s/#username/$un/" config.json || { sed -i "s/$un/#username/" config.json; echo "Failed to update username in config.json, reverted username in file back to dummy."; exit 1; }
-#sleep 1
+echo "Adjusting makeflags for makepkg"
+if ! sed -i "s/#MAKEFLAGS=\"-j2\"/MAKEFLAGS=\"-j$nc\"/g" /etc/makepkg.conf; then
+    echo "Step 1 in adjusting makeflags for makepkg failed"
+    exit 1
+fi
+if ! sed -i "s/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T $nc -z -)/g" /etc/makepkg.conf; then
+    echo "Step 2 in adjusting makeflags for makepkg failed"
+    exit 1
+fi
 
-#read -s -p "Enter password (Input hidden for security): " pd
-#sed -i "s/#password/$pd/" creds.json || { sed -i "s/$pd/#password/" creds.json; echo "Failed to update password in creds.json, reverted password in file back to dummy."; exit 1; }
-#sleep 1
+# read -p "Enter username: " un
+# sed -i "s/#username/$un/" creds.json || { sed -i "s/$un/#username/" creds.json; echo "Failed to update username in creds.json, reverted username in file back to dummy."; exit 1; }
+# sed -i "s/#username/$un/" config.json || { sed -i "s/$un/#username/" config.json; echo "Failed to update username in config.json, reverted username in file back to dummy."; exit 1; }
+# sleep 1
 
-# Update database and check for updates specific to archlinux-keyring, archinstall, reflector and python-setuptools
+# read -s -p "Enter password (Input hidden for security): " pd
+# sed -i "s/#password/$pd/" creds.json || { sed -i "s/$pd/#password/" creds.json; echo "Failed to update password in creds.json, reverted password in file back to dummy."; exit 1; }
+# sleep 1
+
 echo "Updating internal database and checking for updates specific to archlinux-keyring, archinstall, reflector and python-setuptools. Silently skipping if they're already up to date."
 sleep 2
-if ! sudo pacman -Syy --needed archlinux-keyring archinstall reflector python python-setuptools; then
+if ! pacman -Syy --needed archlinux-keyring archinstall reflector python python-setuptools; then
     echo "Failed to update packages"
     exit 1
 fi
 
-# Install with partly generated config
+# --creds creds.json
 echo "Installing with partly generated config in 3.. 2.. 1.."
 sleep 2
 if ! archinstall --config config.json; then
-#if ! archinstall --config config.json --creds creds.json; then
     echo "Failed to install"
     exit 1
 fi
@@ -83,4 +89,4 @@ fi
 echo "APPLYING GTK THEMES TO GTK FLATPAK APPS: itsfoss.com/flatpak-app-apply-theme"
 sleep 2
 
-echo "Done!... hopefully | Post-install script available at github.com/chrishazfun/linux in /arch under extra.sh"
+echo "Done!... hopefully | More to do at github.com/chrishazfun/installarch under extra.sh if you want :)"
