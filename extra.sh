4ff95bc4e6845ced07311d010f6a1124df8e213b
chrishazfun <chrishazfun@users.noreply.github.com>
2023-05-17 20:36:47 +1000

    added both firefox and ms edge to config, small change to note in extra.sh

--- extra.sh
+++ extra.sh
@@ -13,7 +13,7 @@ Hidden=true" > /tmp/1
 find /usr -name "*lsp_plug*desktop" 2>/dev/null | cut -f 5 -d '/' | xargs -I {} cp /tmp/1 ~/.local/share/applications/{}
 find /usr -name "*zam*desktop" 2>/dev/null | cut -f 5 -d '/' | xargs -I {} cp /tmp/1 ~/.local/share/applications/{}
 
-echo "# installing qemu/virt-manager, sed'ing config to "
+echo "# installing qemu/virt-manager, sed'ing config to work"
 pacman -S qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils libguestfs ovmf swtpm openbsd-netcat
 sed -i 's/^#unix_sock_group = \"libvirt\"/unix_sock_group = \"libvirt\"/' /etc/libvirt/libvirtd.conf
 sed -i 's/^#unix_sock_ro_perms = \"0777\"/unix_sock_ro_perms = \"0777\"/' /etc/libvirt/libvirtd.conf
