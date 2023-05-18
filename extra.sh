1629b5d2cf806b8a0940d54cfdfaa766caf508fa
chrishazfun <chrishazfun@users.noreply.github.com>
2023-05-19 08:00:22 +1000

    moved extra aur pkgs to extra.sh except for libappimage/launcher, paru and paruz tui instead of yay and gnome gui

--- extra.sh
+++ extra.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# yay -S --needed --noconfirm greenlight-bin
+paru -S protonup-qt-bin itch-setup-bin heroic-games-launcher-bin mcbelauncher-bin xbox-xcloud flowblade kdocker xboxdrv shutter-encoder
 # ^ eventually probably as xbox-xcloud may be been renamed to greenlight at some point
