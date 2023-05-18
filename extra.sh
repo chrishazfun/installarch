cec511cf65794b9386c2917b59d7f156bb6d8d03
chrishazfun <chrishazfun@users.noreply.github.com>
2023-05-19 09:12:18 +1000

    updates

--- extra.sh
+++ extra.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+
+# read command needed for qemu vm setup
+pacman -S --needed qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils libguestfs ovmf swtpm openbsd-netcat
+sed -i 's/^#unix_sock_group = \"libvirt\"/unix_sock_group = \"libvirt\"/' /etc/libvirt/libvirtd.conf
+sed -i 's/^#unix_sock_ro_perms = \"0777\"/unix_sock_ro_perms = \"0777\"/' /etc/libvirt/libvirtd.conf
+sed -i 's/^#unix_sock_rw_perms = \"0770\"/unix_sock_rw_perms = \"0770\"/' /etc/libvirt/libvirtd.conf
+usermod -aG libvirt $USERNAME
+sudo systemctl enable libvirtd
+
+# read command needed for extra aur pkgs
 paru -S protonup-qt-bin itch-setup-bin heroic-games-launcher-bin mcbelauncher-bin xbox-xcloud flowblade kdocker xboxdrv shutter-encoder
-# ^ eventually probably as xbox-xcloud may be been renamed to greenlight at some point
+
+# xbox-xcloud may be renamed as greenlight or greenlight-bin
