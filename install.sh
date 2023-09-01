3f9b27e44278c9c7999a0aa61908fb6d306bde45
chrishazfun <lowleschris@gmail.com>
2023-09-02 04:18:17 +1000

    fixed condition for nvidia driver importing

--- install.sh
+++ install.sh
@@ -79,7 +79,7 @@ fi
 config="config.json"
 
 # push to nvidia propritary drivers if it's an nvidia card, open-source generic driver on everything else
-if [ 'lspci -k | grep -A 2 -E "(VGA|3D)" | grep -i nvidia | wc -l' = 0 ]; then
+if [ 'lspci -k | grep -A 2 -E "(VGA|3D)" | grep -i nvidia | wc -l' -gt 0 ]; then
 	modified_config=$(jq --arg items "Nvidia (proprietary)" '.profile_config.gfx_driver = $item)' <<< $(cat "$config"))
 	echo "$modified_config" >> temp.json
 	mv temp.json "$config"
